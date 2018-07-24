39f3afa017f59960b3c8b5a75cde59045c27d088
Maru <zhangbinhui@souche.com>
2018-07-25 00:50:02 +0800

    docs: generate docs via jazzy

---
+++ Scripts/generate-docs.sh
@@ -0,0 +1,6 @@
+jazzy \
+--clean \
+--author Maru \
+--author_url https://github.com/Maru-zhang \
+--github_url https://github.com/Maru-zhang/SwiftyEventBus \
+--xcodebuild-arguments -scheme,SwiftyEventBus \
