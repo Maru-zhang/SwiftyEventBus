e8aa0abece8c9b5feae1b29b70352352191fcb7d
Maru <zhangbinhui@souche.com>
2018-07-31 16:57:52 +0800

    chore: change generate doc path

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -4,3 +4,4 @@ jazzy \
 --author_url https://github.com/Maru-zhang \
 --github_url https://github.com/Maru-zhang/SwiftyEventBus \
 --xcodebuild-arguments -scheme,SwiftyEventBus \
+--documentation=Docs/*.md
